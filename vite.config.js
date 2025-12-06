864ab5a79c2b4488b2945276bba75a2fa152d66c
Shritam59 <shritam59@gmail.com>
2025-12-06 12:30:24 +0530

    port number changed to 3001

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000, // 👉 change this to your desired port
+    port: 3001, // 👉 change this to your desired port
     strictPort: true, // optional: forces Vite to use this port only
   },
 })
